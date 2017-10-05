d6328a015b83ea44775201809c4e5815160f5acd
mohan singh <singhmohan333@gmail.com>
2017-10-05 13:37:20 +0530

    map app and p

--- MyPizza/app/src/main/java/com/example/mohan/mypizza/MainActivity.java
+++ MyPizza/app/src/main/java/com/example/mohan/mypizza/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mohan.mypizza;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,14 +24,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,18 +81,30 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.order) {
+            Intent intent = new Intent(MainActivity.this, Contact.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.view_menu) {
+            Intent intent = new Intent(MainActivity.this, MenuItems.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.today_offers) {
+            Intent intent = new Intent(MainActivity.this, Contact.class);
+            startActivity(intent);
+        } else if (id == R.id.extras) {
 
-        } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(MainActivity.this, Contact.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.contact_us) {
 
+            Intent intent = new Intent(MainActivity.this, Contact.class);
+            startActivity(intent);
+
+        } else if (id == R.id.rateapp) {
+            Intent intent = new Intent(MainActivity.this, Contact.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
