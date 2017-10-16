73879199a5a5f8bd6755b7c36aeed7fe1450dd1e
MOHAN SINGH <singhmohan333@gmail.com>
2017-10-16 15:05:13 +0530

    save it

--- AsmTech/app/src/main/java/com/example/mohan/asmtech/Gallery.java
+++ AsmTech/app/src/main/java/com/example/mohan/asmtech/Gallery.java
@@ -17,16 +17,10 @@ public class Gallery extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
     }
 
 
 
-}
+
--- AsmTech/app/src/main/java/com/example/mohan/asmtech/Main2Activity.java
+++ AsmTech/app/src/main/java/com/example/mohan/asmtech/Main2Activity.java
@@ -20,16 +20,10 @@ public class Main2Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
-        });
 
-    }
+
+
         public void onClickb(View view){
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
